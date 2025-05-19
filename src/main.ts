a5ae5bcc84495655d1b2f8fc547f3f56c9c350d4
GaelCast9 <20233tn220@utez.edu.mx>
2025-05-19 00:26:22 -0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5174'], 
     credentials: true,
   });
 
